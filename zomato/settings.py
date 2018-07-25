89958d42848ce049db60dd88955b37a59e87233f
Anish Singh <anish.singh58@gmail.com>
2018-07-25 16:38:46 +0530

    Version FINAL 3

--- zomato/settings.py
+++ zomato/settings.py
@@ -123,6 +123,6 @@ STATIC_URL = '/static/'
 STATIC_ROOT = 'staticfile/'
 
 # has all the static files
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static/'),)
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static/'))
 
 LOGIN_URL = '/login/'
